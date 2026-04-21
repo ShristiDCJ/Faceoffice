69c5fa8e2994da9d80e0aab12a361bf7bb5327c2
Shristi Mishra <shristi.college.mishra@gmail.com>
2026-04-21 16:11:25 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class Employee(db.Model):
     name = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     phone = db.Column(db.String(20), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     face_encoding = db.Column(db.LargeBinary, nullable=False)  # Serialized numpy array
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -76,3 +77,4 @@ class EmployeeFaceLogin(db.Model):
 
     def __repr__(self):
         return f'<EmployeeFaceLogin {self.employee.name}>'
+
